628fb8998ac9a6e1ac14c207fb8b16f7e1b796cc
Shekhar Singh <shekharkrsingh6432@gmail.com>
2026-01-07 22:18:11 +0530

    search in a rotated sorted array with duplicated

--- src/main/java/BinarySearch/Bin1.java
+++ src/main/java/BinarySearch/Bin1.java
@@ -2,9 +2,10 @@ package BinarySearch;
 
 public class Bin1 {
     public static void main(String[] args) {
-        int[] arr= {7, 8, 9, 1, 2, 3, 4, 5, 6};
-        int target=-10;
+        int[] arr= {6, 8, 9, 1, 6, 3, 4, 5, 6};
+        int target=9;
         System.out.println(rotateSortedArray(arr, 0, arr.length-1, target));
+        System.out.println(rotateSortedArrayWithDuplicates(arr, 0, arr.length-1, target));
     }
     private static int rotateSortedArray(int[] arr, int low, int high, int target){
         if(low>high){
@@ -14,6 +15,7 @@ public class Bin1 {
         if(arr[mid]==target){
             return mid;
         }
+        //finding the sorted part and then adding the condition based on that
         if(arr[mid]<=arr[high]){
             if(target>arr[mid] && target<=arr[high]) {
                 return rotateSortedArray(arr, mid + 1, high, target);
@@ -25,6 +27,33 @@ public class Bin1 {
                 return rotateSortedArray(arr, low, mid-1, target);
             }else{
                 return rotateSortedArray(arr, mid+1, high, target);
+            }
+        }
+    }
+    private static boolean rotateSortedArrayWithDuplicates(int[] arr, int low, int high, int target){
+        if(low>high){
+            return false;
+        }
+        int mid=low+(high-low)/2;
+        if(arr[mid]==target){
+            return true;
+        }
+        //shrinking the search boundary
+        if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+            low++;
+            high--;
+        }
+        if(arr[mid]<=arr[high]){
+            if(target>arr[mid] && target<=arr[high]) {
+                return rotateSortedArrayWithDuplicates(arr, mid + 1, high, target);
+            }else{
+                return rotateSortedArrayWithDuplicates(arr, low, mid-1, target);
+            }
+        }else{
+            if(target>arr[low] && target<=arr[mid]) {
+                return rotateSortedArrayWithDuplicates(arr, low, mid-1, target);
+            }else{
+                return rotateSortedArrayWithDuplicates(arr, mid+1, high, target);
             }
         }
     }
