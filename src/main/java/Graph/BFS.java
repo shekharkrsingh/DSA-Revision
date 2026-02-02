529d73c1625c1b46a0b3ac49e95c2c2c0e434d5d
Shekhar Singh <shekharkrsingh6432@gmail.com>
2026-02-03 05:02:42 +0530

    graph bfs

---
+++ src/main/java/Graph/BFS.java
@@ -0,0 +1,49 @@
+package Graph;
+
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        int n=10;
+        List<List<Integer>> adj= new ArrayList<>();
+        adj.add(List.of());
+        adj.add(Arrays.asList(2,6));
+        adj.add(Arrays.asList(1,3,4));
+        adj.add(List.of(2));
+        adj.add(Arrays.asList(2,5));
+        adj.add(Arrays.asList(4,8));
+        adj.add(Arrays.asList(1,7,9));
+        adj.add(Arrays.asList(6,8));
+        adj.add(Arrays.asList(5,7));
+        adj.add(List.of());
+        System.out.println(bfs(adj, 10));
+    }
+
+    private static List<Integer> bfs(List<List<Integer>> adj, int n){
+        List<Boolean> visited=new ArrayList<>();
+        Queue<Integer> qu=new ArrayDeque<>();
+        List<Integer> result= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            visited.add(false);
+        }
+        for(int i=1;i<n;i++){
+            if(!visited.get(i)){
+                qu.offer(i);
+                visited.set(i, true);
+                while(!qu.isEmpty()){
+                    int node= qu.poll();
+                    result.add(node);
+                    visited.set(node, true);
+                    List<Integer> list= adj.get(node);
+                    for(int m: list){
+                        if(!visited.get(m)){
+                            visited.set(m, true);
+                            qu.offer(m);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
